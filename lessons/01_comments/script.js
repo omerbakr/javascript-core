c85cc92952a1523c7a687a67be264cf15eebee4e
omerbakr <y.omerbakir@gmail.com>
2026-01-30 18:57:41 +0300

    add comments fundamentals

---
+++ lessons/01_comments/script.js
@@ -0,0 +1,41 @@
+/*
+  Comments do not affect code execution.
+  They exist to help developers understand decisions.
+*/
+
+// Single-line comments
+// Short notes or temporary debugging
+const userAge = 25; // User's current age
+
+// console.log("This line will not run");
+
+// Multi-line comments
+/*
+  Used to explain complex ideas
+  or to disable blocks of code.
+*/
+
+// Tips for Writing Great Comments
+// 1. Be Concise but Clear: Avoid writing long paragraphs. Instead, focus on key points.
+
+// Bad comment ❌
+// This function takes two numbers as input, adds them together, and then returns the result.
+
+// Good comment ✅
+// Adds two numbers
+
+// 2. Explain the Why, Not Just the What
+
+// Bad comment ❌
+// Increment the value
+let value = 0;
+value++;
+
+// Good comment ✅
+// Increment value to reflect the next step in the process
+value++;
+
+// 3. Use TODO and FIXME
+
+// TODO: Add input validation
+// FIXME: Fix edge case here
